3708d124e1e69bfa903320271ca1ca76204de9b0
ryan-caufield <rmc010@mail.usask.ca>
2018-06-06 15:35:30 -0400

    Add files via upload

--- SerialCommunication.py
+++ SerialCommunication.py
@@ -4,10 +4,11 @@ import sys
 import atexit
 
 class SerialCom:
-    
-    def __init__(self, data_queue, sensor_value):
+
+    def __init__(self, data_queue, vial_list, vial_q):
         self.data_q = data_queue
-        self.sensor_value = sensor_value
+        self.vial_list = vial_list
+        self.vial_q = vial_q
 
         ports = list(serial.tools.list_ports.comports()) # Detect arduino serial port
         for p in ports:
@@ -16,8 +17,8 @@ class SerialCom:
         self.ser = serial.Serial(SERIAL, 9600)
         atexit.register(self.shutdown) # on exit, resets the arduino, putting it back into standby mode
 
-    def setup_serial(self, temperature, mode):
-        
+    def setup_serial(self, mode, temperature, sensor_value):
+
         self.ser.setDTR(False) # pulsing DTR resets the arduino
         sleep(0.022)
         self.ser.setDTR(True)
@@ -25,38 +26,31 @@ class SerialCom:
         sleep(5) # wait for initialization
 
         for x in range(0, 9):
-            if int(self.sensor_value[x])<100 and int(self.sensor_value[x])>9:
-                self.sensor_value[x]= str(str(0) + str(self.sensor_value[x]))
-            elif int(self.sensor_value[x])<10:
-                self.sensor_value[x]= str(str(0) + str(0) + str(self.sensor_value[x]))
-        ser_write = str(int(mode.value))
+            if int(sensor_value[x])<100 and int(sensor_value[x])>9:
+                sensor_value[x]= str(str(0) + str(sensor_value[x]))
+            elif int(sensor_value[x])<10:
+                sensor_value[x]= str(str(0) + str(0) + str(sensor_value[x]))
+        ser_write = 'r' + 'm' + str(int(mode.value))
         for x in range(0, 9):
-            ser_write = ser_write + str(int(temperature[x])) + str(self.sensor_value[x])
+            ser_write = ser_write + 't' + str(x) + str(int(temperature[x])) + 'v' + str(x) + str(sensor_value[x])
         self.ser.write(ser_write)
         sleep(2)
 
 
-    def arduino(self,temperature,mode):
-        self.setup_serial(temperature, mode)
-        #flag = 0
+    def arduino(self,mode,new_mode,temperature,new_temperature,sensor_value,new_sensor_value,heat,new_heat,stir,new_stir,pump_in,new_pump_in,pump_out,new_pump_out,led_od,new_led_od):
+        self.setup_serial(mode,temperature,sensor_value)
         while True:
-            ''' For testing the mode select
-            flag = flag + 1
-            if (flag >= 100):
-                flag = 0
-                if (mode.value == 1):
-                    mode.value = 0
-                else:
-                    mode.value = 1
-            '''
             if self.ser.inWaiting():
-                #print('Receiving:')
                 line = self.ser.readline()
                 self.ser.flushInput()
                 #data = [float(val) for val in line.split()]
                 data = line.split()
+                #print('Receiving:')
+                #print(data)
                 sleep(0.1)
                 if (len(data) == 10):
+                    print data
+                    self.vial_q.put(data)
                     if(int(data[0]) == 1):
                         print ('1 temperature sensor detected.')
                     else:
@@ -65,15 +59,42 @@ class SerialCom:
                         if (str(data[x+1]) != '0000000000000000'):
                             print('    Vial ' + str(x) + ' Address:  ' + str(data[x+1]))
                 else:
-                    self.data_q.put(data)
-                    ser_write = str(int(mode.value))
+                    self.data_q.put(data) # save data in the queue
+
+                    ser_write = '0'
+                    if (mode.value != new_mode.value):
+                        mode.value = new_mode.value
+                        ser_write = 'm' + str(int(mode.value))
                     for x in range(0, 9):
-                        ser_write = ser_write + str(int(temperature[x])) + str(self.sensor_value[x])
-                    #print('Sending:')
-                    #print(ser_write)
-                    #print('Length: ' + str(len(ser_write)))
-                    self.ser.write(ser_write)
-                    sleep(2)
+                        if (temperature[x] != new_temperature[x]):
+                            temperature[x] = new_temperature[x]
+                            ser_write = ser_write + 't' + str(x) + str(int(temperature[x]))
+                        if (sensor_value[x] != new_sensor_value[x]):
+                            sensor_value[x] = new_sensor_value[x]
+                            ser_write = ser_write + 'v' + str(x) + str(int(sensor_value[x]))
+                        if (heat[x] != new_heat[x]):
+                            heat[x] = new_heat[x]
+                            ser_write = ser_write + 'h' + str(x) + str(int(heat))
+                        if (stir[x] != new_stir[x]):
+                            stir[x] = new_stir[x]
+                            ser_write = ser_write + 's' + str(x) + str(int(stir[x]))
+                        if (pump_in[x] != new_pump_in[x]):
+                            pump_in[x] = new_pump_in[x]
+                            ser_write = ser_write + 'i' + str(x) + str(int(pump_in[x]))
+                        if (pump_out[x] != new_pump_out[x]):
+                            pump_out[x] = new_pump_out[x]
+                            ser_write = ser_write + 'o' + str(x) + str(int(pump_out[x]))
+                        if (led_od[x] != new_led_od[x]):
+                            led_od[x] = new_led_od[x]
+                            ser_write = ser_write + 'l' + str(x) + str(int(led_od[x]))
+                    if (ser_write != '0'):
+                        #print('Sending:')
+                        #print(ser_write)
+                        #print('Length: ' + str(len(ser_write)))
+                        self.ser.write(ser_write) # send serial
+                        sleep(2)
+
+
 
     def shutdown(self):
         self.ser.setDTR(False) # pulsing DTR resets the arduino
--- main_kivy.py
+++ main_kivy.py
@@ -8,7 +8,7 @@
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
@@ -19,16 +19,63 @@ from threading import Thread
 from kivy.clock import Clock
 from time import sleep
 from kivy.uix.progressbar import ProgressBar
-# import sys, serial, argparse
-import numpy as np
-from collections import deque
-from multiprocessing import Queue, Process
-from Queue import Empty
+# import argparse
+import sys
+from time import sleep
+from multiprocessing import Queue, Process, Value, Array, Event
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-# import serial.tools.list_ports
-import math
+from ctypes import c_char_p
+import Calibrations
+import SerialCommunication
+import AnalogPlot
+
+'''User Inputs______________________________________________________________________________________________________________________________________________
+
+Vial #:            [0] [1] [2] [3] [4] [5] [6] [7] [8]'''
+Nonereading_LB  = [716,840,704,900,900,900,900,900,900]
+OD_value        = [0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8]     # desired OD sensor value (3 digits, 0.1-2.0)
+Temperature_Set = [ 37, 37, 37, 37, 37, 37, 37, 37, 37]     # desired vial temperature (2 digits)
+
+maxLen          = 100   # length of x-axis on plots
+Chemostat_Mode  = 1     # 1 for experiment, 2 for pump, 3 for test
+
+'''_________________________________________________________________________________________________________________________________________________________'''
+
+empty_list = [0, 0, 0, 0, 0, 0, 0, 0, 0] # initial values for below arrays
+vial_addresses = ['99','0000000000000000','0000000000000000','0000000000000000','0000000000000000','0000000000000000','0000000000000000','0000000000000000','0000000000000000','0000000000000000']
+# Values stored in shared memory, which can be accessed inside of any parallel process
+mode = Value('d', Chemostat_Mode)
+new_mode = Value('d', Chemostat_Mode)
+temperature = Array('i', Temperature_Set)
+new_temperature = Array('i', Temperature_Set)
+sensor_value = Array('i', empty_list)
+new_sensor_value = Array('i', empty_list)
+heat = Array('i', empty_list)
+new_heat = Array('i', empty_list)
+stir = Array('i', empty_list)
+new_stir = Array('i', empty_list)
+pump_in = Array('i', empty_list)
+new_pump_in = Array('i', empty_list)
+pump_out = Array('i', empty_list)
+new_pump_out = Array('i', empty_list)
+led_od = Array('i', empty_list)
+new_led_od = Array('i', empty_list)
+vial_list = Array(c_char_p,vial_addresses)
+vial_q = Queue()
+
+'''
+# Use to test serial communication - temperature set point should spike up and down
+def serialTest (new_temperature):
+    while True:
+        for x in range(9):
+            if ( new_temperature[x] > 44 ):
+                new_temperature[x] = 37
+            else:
+                new_temperature[x] = new_temperature[x] + 1
+        sleep(2)
+'''
 
 exp_pressed = 0 # 1 when experiment is running, 0 when stopped
 
@@ -46,29 +93,44 @@ def get_data():
         levels.insert(0, data)
         sleep(0.2)
 
-
 class SetupScreen(Screen):
 
     #start progress bar by initiating clock schedule
-    def progress_start(self):
-        Clock.schedule_interval(self.progress_loop,1/60) #runs function every 1/60 sec
+    def setup_process(self):
+        Clock.schedule_interval(self.progress_loop,0.12)
+        serialcom = SerialCommunication.SerialCom(data_q,vial_list,vial_q)
+        global arduino_process
+        arduino_process = Process(target = serialcom.arduino, args = (mode,new_mode,temperature,new_temperature,sensor_value,new_sensor_value,heat,new_heat,stir,new_stir,pump_in,new_pump_in,pump_out,new_pump_out,led_od,new_led_od,))
+        arduino_process.start()
 
     #progress bar schedule function
-    def progress_loop(self, dt):
+    def progress_loop(self,dt):
         setup_progress = self.ids.setup_progress
 
-        #Check if progress bar is @ max; if not increass progress by 1
+        #Check if progress bar is @ max; if not increase progress by 1
         if setup_progress.value >= setup_progress.max:
             Clock.unschedule(self.progress_loop) #Stops loop
-            #Transition to another Screen
+            self.vial_detector()
+            self.ids.cont.disabled = False
+        else:
+            setup_progress.value += 1
 
-            self.go_home()
-            return False
-        setup_progress.value += 1
+    def vial_detector(self):
+        vials_label = self.ids.vials_label
+        addresses_label = self.ids.vial_addresses
+        while (vial_q.empty()):
+            pass
+        vial_list = vial_q.get()
+        if (vial_list[0] == '1'):
+            vials_label.text = "1 Vial Detected"
+        else:
+            vials_label.text = vial_list[0] + " Vials Detected"
+        addresses_label.text = "Vial Addresses: "
+        for x in range (1,9):
+            if(str(vial_list[x]) != '0000000000000000'):
+                addresses_label.text = addresses_label.text + "\nVial " + str(x) + " Address:  " + str(vial_list[x])
 
-    def go_home(self):
-        App.get_running_app().transition = SlideTransition(direction = 'up')
-        App.get_running_app().root.current = 'home'
+
 
 class HomeScreen(Screen):
 
@@ -100,14 +162,12 @@ class HomeScreen(Screen):
             self.ids.graph.add_plot(self.stir_plot)
             self.ids.graph.add_plot(self.mot_in_plot)
             self.ids.graph.add_plot(self.mot_out_plot)
-            self.event = Clock.schedule_interval(self.get_value, 0.2) # starts plotting values
-
+            Clock.schedule_interval(self.get_value, 0.2) # starts plotting values
         else:
             exp_pressed = 0
             exp.background_color = [0,0.8,0,1]
             exp.text = "Start Experiment"
-            self.event.cancel()
-
+            Clock.unschedule(self.get_value)
 
     def get_value(self, dt):
         self.temp_plot.points = [(i, j) for i, j in enumerate(levels)]
@@ -121,26 +181,14 @@ class HomeScreen(Screen):
         self.mot_out_plot.points = [(i, j+20) for i, j in enumerate(levels)]
 
     def vial(self, vial_number): # changes the active vial
-        vial_label = self.ids.vial_number
-        vial_label.text = str(vial_number)
+        self.ids.vial_number.text = str(vial_number)
 
-    def go_setup(self, root_manager):
-        root_manager.get_screen('setup').ids.setup_progress.value = 0
-        App.get_running_app().transition = SlideTransition(direction = 'down')
-        App.get_running_app().root.current = 'setup'
-
-    def go_manual(self):
-        App.get_running_app().transition = SlideTransition(direction = 'left')
-        App.get_running_app().root.current = 'manual'
-
-    def go_setting(self):
-        App.get_running_app().transition = SlideTransition(direction = 'up')
-        App.get_running_app().root.current = 'settings'
+    def exit(self):
+        arduino_process.terminate()
+        App.get_running_app().stop()
 
 class ManualScreen(Screen):
-    def go_home(self):
-        App.get_running_app().transition = SlideTransition(direction = 'right')
-        App.get_running_app().root.current = 'settings'
+    pass
 
 class SettingsScreen(Screen):
     pass
@@ -157,12 +205,27 @@ class MainButton(Button):
 class DataLabel(Label):
     pass
 
-class Main_KivyApp(App):
+class MainApp(App):
     pass
 
 if __name__ == "__main__":
+
+
+
+    sensor_value = [100,100,100,100,100,100,100,100,100] # Initailize sensor_value array
+
+    #test_process = Process(target = serialTest, args = (new_temperature,))
+    #test_process.start()
+
+    Cali = Calibrations.Calibrations()
+    for i in range(0,9):
+        sensor_value[i] = Cali.OD_to_Reading(Nonereading_LB[i],OD_value[i]) # Setup population level
+
+    data_q = Queue() # Queue that holds all data from arduino
+
     Window.clearcolor = (0.5, 0.5, 0.5, 1) # sets background colour
     get_data_thread = Thread(target = get_data)
     get_data_thread.daemon = True
     get_data_thread.start()
-    Main_KivyApp().run()
+    MainApp().run()
+    print 'Goodbye :)'
